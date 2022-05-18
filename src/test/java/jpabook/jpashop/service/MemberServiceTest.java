9a02047bb2556aa83a87566db8a76cf6d93f2fa6
Hyeonw <hyeonw777@naver.com>
2022-05-18 16:54:59 +0900

    회원 서비스 테스트 완료

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+//    @Rollback(false)
+    public void 회원가입() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("Kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Assertions.assertEquals(member, memberRepository.findOne(saveId));
+    }
+    
+    @Test()
+    public void 중복_회원_예외() throws Exception{
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+
+        //then
+        IllegalStateException thrown = Assertions.assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        Assertions.assertEquals("이미 존재하는 회원입니다.", thrown.getMessage());
+    }
+
+}
